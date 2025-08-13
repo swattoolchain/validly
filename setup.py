f8cfe8da8b232d2efb747c57b97ae5df56eef0b8
swattoolchain <167977302+swattoolchain@users.noreply.github.com>
2025-08-13 22:37:28 +0530

    version_update

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name='Validly',
-    version='1.0.0',
-    author='Your Name',
-    author_email='your.email@example.com',
+    version='1.0.1',
+    author='Dinesh RVL',
+    author_email='swat.github@gmail.com',
     description='A powerful and extensible data validation and comparison tool for developers and testers.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/your-github-username/Validly',
+    url='https://github.com/swat-github/Validly',
     packages=find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Intended Audience :: Developers',
-        'Intended Audience :: Quality Assurance',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Testing'
     ],
     python_requires='>=3.6',
