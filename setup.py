4384d79f956a93a683822556ff59c94379e5dc7b
curler-auto <premitive.solutions@gmail.com>
2025-09-08 15:49:02 +0530

    version upgrade

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Validly',
-    version='1.0.9',
+    version='1.0.14',
     author='Dinesh RVL',
     author_email='swat.github@gmail.com',
     description='A powerful and extensible data validation and comparison tool for developers and testers.',
@@ -19,5 +19,5 @@ setup(
         'Topic :: Software Development :: Testing'
     ],
     python_requires='>=3.6',
-    install_requires=[]
+    install_requires=['requests']
 )
